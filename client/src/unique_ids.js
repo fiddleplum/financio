1dd78f2236001cca30dbffe2e5fc0b45e9b828b6
Stephen Hurley <stephenhurley@gmail.com>
2018-08-06 15:24:53 -0700

    Fixed up eslintrc and added more comments.

--- client/src/unique_ids.js
+++ client/src/unique_ids.js
@@ -1,10 +1,30 @@
+/**
+ * A generator of ids that are unique per instance of the class.
+ */
 class UniqueIds {
+	/**
+	 * Constructor.
+	 */
 	constructor() {
+		/**
+		 * The free ids. If there are no free ids, the next free id is the number used.
+		 * @type {number}
+		 * @private
+		 */
 		this._freeIds = [];
 
+		/**
+		 * The number of used ids.
+		 * @type {number}
+		 * @private
+		 */
 		this._numUsed = 0;
 	}
 
+	/**
+	 * Gets a unique id.
+	 * @returns {number}
+	 */
 	get() {
 		let id;
 		if (this._freeIds.length === 0) {
@@ -17,6 +37,10 @@ class UniqueIds {
 		return id;
 	}
 
+	/**
+	 * Releases a previously gotten id.
+	 * @param {number} id
+	 */
 	release(id) {
 		this._freeIds.push(id);
 		this._numUsed -= 1;
--- client/src/ws.js
+++ client/src/ws.js
@@ -1,6 +1,12 @@
 import UniqueIds from './unique_ids';
 
 /**
+ * This callback is displayed as a global member.
+ * @callback receivedPromiseResolve
+ * @param {*} data
+ */
+
+/**
  * A message sending and receiving WebSocket interface.
  */
 class WS {
@@ -27,24 +33,36 @@ class WS {
 		this._uniqueIds = new UniqueIds();
 
 		/**
-		 * The list of active sent messages awaitin a received message.
-		 * @type {Map<number, function()>}
+		 * The list of active sent messages awaiting a received message.
+		 * @type {Map<number, receivedPromiseResolve>}
 		 * @private
 		 */
 		this._activeSends = new Map();
 
-		// Initialize.
+		// Create the web socket.
 		this._webSocket = new WebSocket('ws:' + url);
 
+		// Setup the promise that resolves when the web socket is ready to be used.
 		this._readyPromise = new Promise((resolve, reject) => {
 			this._webSocket.onopen = resolve;
 		});
 
+		// When a message is received...
 		this._webSocket.onmessage = (message) => {
 			console.log('received ' + message.data);
+
+			// Get the json and the id.
 			let json = JSON.parse(message.data);
-			let resolve = this._activeSends.get(json.id);
-			this._activeSends.delete(json.id);
+			let id = json.id;
+
+			// Get the function to be resolved using the id in the json.
+			let resolve = this._activeSends.get(id);
+
+			// Remove the id from the actively sending message list and release the id.
+			this._activeSends.delete(id);
+			this._uniqueIds.release(id);
+
+			// Call the resolve function.
 			resolve(json.data);
 		};
 		window.addEventListener('beforeunload', () => {
