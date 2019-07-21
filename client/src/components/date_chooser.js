de963dc319cacdc527f2bb42ebffd06105b121e4
Stephen Hurley <stephenhurley@gmail.com>
2019-07-20 20:53:45 -0700

    Formatted date chooser nicely. (2)

--- client/src/components/date_chooser.js
+++ client/src/components/date_chooser.js
@@ -97,89 +97,89 @@ export default class DateChooser extends Component {
 }
 
 DateChooser.html = `
-	<div id="decade_dec" class="clickable inline-vertical-align">
+	<div id="decade_dec" class="clickable vertical-align">
 		<svg viewBox="0 0 32 32">
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="20,4 4,16 20,27" />
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="28,4 12,16 28,27" />
 		</svg>
 	</div>
-	<div id="year_dec" class="clickable inline-vertical-align">
+	<div id="year_dec" class="clickable vertical-align">
 		<svg viewBox="0 0 32 32">
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="24,4 8,16 24,27" />
 		</svg>
 	</div>
-	<div id="year_current" class="inline-vertical-align">{{year}}</div>
-	<div id="year_inc" class="clickable inline-vertical-align">
+	<div id="year_current" class="vertical-align">{{year}}</div>
+	<div id="year_inc" class="clickable vertical-align">
 		<svg viewBox="0 0 32 32">
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="8,4 24,16 8,27" />
 		</svg>
 	</div>
-	<div id="decade_inc" class="clickable inline-vertical-align">
+	<div id="decade_inc" class="clickable vertical-align">
 		<svg viewBox="0 0 32 32">
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="12,4 28,16 12,27" />
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="4,4 20,16 4,27" />
 		</svg>
 	</div>
-	<div id="month_dec" class="clickable inline-vertical-align">
+	<div id="month_dec" class="clickable vertical-align">
 		<svg viewBox="0 0 32 32">
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="24,4 8,16 24,27" />
 		</svg>
 	</div>
-	<div id="month_current" class="inline-vertical-align">{{month}}</div>
-	<div id="month_inc" class="clickable inline-vertical-align">
+	<div id="month_current" class="vertical-align">{{month}}</div>
+	<div id="month_inc" class="clickable vertical-align">
 		<svg viewBox="0 0 32 32">
 			<polyline stroke="var(--fg-light)" stroke-width="2" fill="none" points="8,4 24,16 8,27" />
 		</svg>
 	</div>
-	<div class="inline-vertical-align week">S</div>
-	<div class="inline-vertical-align week">M</div>
-	<div class="inline-vertical-align week">T</div>
-	<div class="inline-vertical-align week">W</div>
-	<div class="inline-vertical-align week">T</div>
-	<div class="inline-vertical-align week">F</div>
-	<div class="inline-vertical-align week">S</div>
-	<div id="day_0" class="clickable inline-vertical-align"></div>
-	<div id="day_1" class="clickable inline-vertical-align"></div>
-	<div id="day_2" class="clickable inline-vertical-align"></div>
-	<div id="day_3" class="clickable inline-vertical-align"></div>
-	<div id="day_4" class="clickable inline-vertical-align"></div>
-	<div id="day_5" class="clickable inline-vertical-align"></div>
-	<div id="day_6" class="clickable inline-vertical-align"></div>
-	<div id="day_7" class="clickable inline-vertical-align"></div>
-	<div id="day_8" class="clickable inline-vertical-align"></div>
-	<div id="day_9" class="clickable inline-vertical-align"></div>
-	<div id="day_10" class="clickable inline-vertical-align"></div>
-	<div id="day_11" class="clickable inline-vertical-align"></div>
-	<div id="day_12" class="clickable inline-vertical-align"></div>
-	<div id="day_13" class="clickable inline-vertical-align"></div>
-	<div id="day_14" class="clickable inline-vertical-align"></div>
-	<div id="day_15" class="clickable inline-vertical-align"></div>
-	<div id="day_16" class="clickable inline-vertical-align"></div>
-	<div id="day_17" class="clickable inline-vertical-align"></div>
-	<div id="day_18" class="clickable inline-vertical-align"></div>
-	<div id="day_19" class="clickable inline-vertical-align"></div>
-	<div id="day_20" class="clickable inline-vertical-align"></div>
-	<div id="day_21" class="clickable inline-vertical-align"></div>
-	<div id="day_22" class="clickable inline-vertical-align"></div>
-	<div id="day_23" class="clickable inline-vertical-align"></div>
-	<div id="day_24" class="clickable inline-vertical-align"></div>
-	<div id="day_25" class="clickable inline-vertical-align"></div>
-	<div id="day_26" class="clickable inline-vertical-align"></div>
-	<div id="day_27" class="clickable inline-vertical-align"></div>
-	<div id="day_28" class="clickable inline-vertical-align"></div>
-	<div id="day_29" class="clickable inline-vertical-align"></div>
-	<div id="day_30" class="clickable inline-vertical-align"></div>
-	<div id="day_31" class="clickable inline-vertical-align"></div>
-	<div id="day_32" class="clickable inline-vertical-align"></div>
-	<div id="day_33" class="clickable inline-vertical-align"></div>
-	<div id="day_34" class="clickable inline-vertical-align"></div>
-	<div id="day_35" class="clickable inline-vertical-align"></div>
-	<div id="day_36" class="clickable inline-vertical-align"></div>
-	<div id="day_37" class="clickable inline-vertical-align"></div>
-	<div id="day_38" class="clickable inline-vertical-align"></div>
-	<div id="day_39" class="clickable inline-vertical-align"></div>
-	<div id="day_40" class="clickable inline-vertical-align"></div>
-	<div id="day_41" class="clickable inline-vertical-align"></div>
+	<div class="vertical-align week">S</div>
+	<div class="vertical-align week">M</div>
+	<div class="vertical-align week">T</div>
+	<div class="vertical-align week">W</div>
+	<div class="vertical-align week">T</div>
+	<div class="vertical-align week">F</div>
+	<div class="vertical-align week">S</div>
+	<div id="day_0" class="clickable vertical-align"></div>
+	<div id="day_1" class="clickable vertical-align"></div>
+	<div id="day_2" class="clickable vertical-align"></div>
+	<div id="day_3" class="clickable vertical-align"></div>
+	<div id="day_4" class="clickable vertical-align"></div>
+	<div id="day_5" class="clickable vertical-align"></div>
+	<div id="day_6" class="clickable vertical-align"></div>
+	<div id="day_7" class="clickable vertical-align"></div>
+	<div id="day_8" class="clickable vertical-align"></div>
+	<div id="day_9" class="clickable vertical-align"></div>
+	<div id="day_10" class="clickable vertical-align"></div>
+	<div id="day_11" class="clickable vertical-align"></div>
+	<div id="day_12" class="clickable vertical-align"></div>
+	<div id="day_13" class="clickable vertical-align"></div>
+	<div id="day_14" class="clickable vertical-align"></div>
+	<div id="day_15" class="clickable vertical-align"></div>
+	<div id="day_16" class="clickable vertical-align"></div>
+	<div id="day_17" class="clickable vertical-align"></div>
+	<div id="day_18" class="clickable vertical-align"></div>
+	<div id="day_19" class="clickable vertical-align"></div>
+	<div id="day_20" class="clickable vertical-align"></div>
+	<div id="day_21" class="clickable vertical-align"></div>
+	<div id="day_22" class="clickable vertical-align"></div>
+	<div id="day_23" class="clickable vertical-align"></div>
+	<div id="day_24" class="clickable vertical-align"></div>
+	<div id="day_25" class="clickable vertical-align"></div>
+	<div id="day_26" class="clickable vertical-align"></div>
+	<div id="day_27" class="clickable vertical-align"></div>
+	<div id="day_28" class="clickable vertical-align"></div>
+	<div id="day_29" class="clickable vertical-align"></div>
+	<div id="day_30" class="clickable vertical-align"></div>
+	<div id="day_31" class="clickable vertical-align"></div>
+	<div id="day_32" class="clickable vertical-align"></div>
+	<div id="day_33" class="clickable vertical-align"></div>
+	<div id="day_34" class="clickable vertical-align"></div>
+	<div id="day_35" class="clickable vertical-align"></div>
+	<div id="day_36" class="clickable vertical-align"></div>
+	<div id="day_37" class="clickable vertical-align"></div>
+	<div id="day_38" class="clickable vertical-align"></div>
+	<div id="day_39" class="clickable vertical-align"></div>
+	<div id="day_40" class="clickable vertical-align"></div>
+	<div id="day_41" class="clickable vertical-align"></div>
 	`;
 
 DateChooser.style = `
